545568a9fc65a77b22b9838f76a8b4fb289cfc53
Sanjana Bhat <sanjanabhat2103@gmail.com>
2026-08-18 14:35:22 +0530

    Step 20: r_squared

--- model.py
+++ model.py
@@ -197,8 +197,11 @@ def mean_absolute_error(y_true, y_pred):
 def root_mean_squared_error(y_true, y_pred):
     return np.sqrt(mse_loss(y_true, y_pred))
 
-# Step 20 - r_squared (not yet solved)
-# TODO: implement
+# Step 20 - r_squared
+def r_squared(y_true, y_pred):
+    ss_res = np.sum((y_true - y_pred) ** 2)
+    ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
+    return 1 - ss_res / ss_tot if ss_tot != 0.0 else np.nan
 
 # Step 21 - evaluate_regression (not yet solved)
 # TODO: implement
